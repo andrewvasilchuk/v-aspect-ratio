5d3b219516239ddf1ed7583ada2ed2c85c222818
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-09-20 17:59:51 +0300

    BREAKING CHANGE: remove checks for a valid value

--- src/helpers.js
+++ src/helpers.js
@@ -1,27 +1,4 @@
-import config from './config'
-
 export function applyDirective(value, style) {
-  if (!value) {
-    throw new Error('No value provided')
-  }
-
-  if (typeof value !== 'string') {
-    throw new Error(
-      `Expected directive binding value type to be a string but found a ${typeof value} instead`
-    )
-  }
-
-  if (!config.pattern.test(value)) {
-    throw new Error(
-      `Expected directive value to match pattern "[Integer]:[Integer]" gut got a ${value}`
-    )
-  }
-
-  const values = value.split(':').map(Number)
-
-  if (values[0] === 0) {
-    throw new Error('Division by 0')
-  }
-
-  style.paddingBottom = `${(values[1] / values[0]) * 100}%`
+  const [x, y] = value.split(':').map(Number)
+  style.paddingBottom = `${(y / x) * 100}%`
 }
