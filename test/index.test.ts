e32fac4677921b8ad897fd23ee0a98855bcebb7b
andrewvasilchuk <andrew.d.vasilchuk@gmail.com>
2020-09-21 23:37:13 +0300

    test: rewrite

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,28 +1,22 @@
 import { TestManager } from './util'
 
-describe('directive usage', () => {
+describe('v-aspect-ratio', () => {
   it('should correctly set aspect ratio', () => {
-    const [a, b] = [16, 9]
     const wrapper = TestManager.createWrapper({
-      template: `<div v-aspect-ratio="\`${a}:${b}\`"></div>`,
+      propsData: { aspectRatio: `16:9` },
     })
-    expect(wrapper.element.style.paddingBottom).toBe(`${(b / a) * 100}%`)
+    expect(wrapper.element.style.paddingBottom).toBe('56.25%')
   })
 
-  it('should correctly set aspect ratio when value is updated', () => {
-    const [a, b] = [4, 3]
-
+  it('should correctly set aspect ratio when value is updated', (done) => {
     const wrapper = TestManager.createWrapper({
-      template: `<div v-aspect-ratio="aspectRatio"></div>`,
-      data() {
-        return {
-          aspectRatio: '16:9',
-        }
-      },
+      propsData: { aspectRatio: '16:9' },
     })
-
-    wrapper.setData({ aspectRatio: `${a}:${b}` })
-
-    expect(wrapper.element.style.paddingBottom).toBe(`${(b / a) * 100}%`)
+    expect(wrapper.element.style.paddingBottom).toBe('56.25%')
+    wrapper.vm.value = '4:3'
+    wrapper.vm.$nextTick(() => {
+      expect(wrapper.element.style.paddingBottom).toBe('75%')
+      done()
+    })
   })
 })
